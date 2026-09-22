a065e9dc7c0b7fdfa44eebac4c01ee1f0768d936
anusrikota12 <anusrikota68@gmail.com>
2026-09-22 12:07:00 +0530

    Initial commit

---
+++ employee.js
@@ -0,0 +1,14 @@
+let company="Infosys";
+function employeeDetails() {
+    let employee="Kota Anu Sri";
+    console.log("Company: "+company);
+    console.log("Employee: "+employee);
+    if(true){
+        let salary=60000;
+        const department="IT";
+        console.log("Salary: "+salary);
+        console.log("Department: "+department);
+    }
+}
+employeeDetails();
+console.log("Company: "+company);
